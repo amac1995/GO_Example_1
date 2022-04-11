8c00a5d66ed61d926890c6942eb6bfe7523ab9d5
amac1995 <andre_correia_87@hotmail.com>
2022-04-11 11:34:07 +0100

    Add files via upload

---
+++ package main.go
@@ -0,0 +1,17 @@
+//é necessário para saber onde está o main
+package main
+
+// Import block, it allows multiple packages to be imported without
+// repeating the "import" keyword
+
+import (
+	"fmt"
+)
+
+/*
+Função main
+When part of the main package, identifies the entry point of an application
+*/
+func main() {
+	fmt.Println("Hello, playground")
+}
