77735efb2a26ec183c3b8136c15028884688ac62
André <andre._correia_87@hotmail.com>
2022-04-20 17:16:52 +0100

    1.1 - Switch Statements

---
+++ programsCRUD/switch.go
@@ -0,0 +1,23 @@
+package programscrud
+
+type HTTPRequest struct {
+	Method string
+}
+
+func main() {
+	r := HTTPRequest{Method: "GET"}
+
+	switch r.Method {
+	case "GET":
+		println("Get Request")
+	case "DELETE":
+		println("Delete Request")
+	case "POST":
+		println("Post Request")
+	case "PUT":
+		println("Put Request")
+	default:
+		println("Unhandled method")
+	}
+
+}
